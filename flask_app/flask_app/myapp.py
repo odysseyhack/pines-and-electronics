65404e74ab3692c391a8d87b7f1838cdfbf9176a
dujm <jdu523@outlook.com>
2019-04-13 21:02:47 +0100

    update debugging snap command

--- flask_app/flask_app/myapp.py
+++ flask_app/flask_app/myapp.py
@@ -1,15 +1,15 @@
-from flask_app.camera.gcp_vision_multi_img import proccess_picture
+#from flask_app.camera.gcp_vision_multi_img import proccess_picture
 from flask import Flask
 import subprocess
-from squid_py.ddo.metadata import Metadata
-from squid_py.ocean import Ocean
-from squid_py.config import Config
+#from squid_py.ddo.metadata import Metadata
+#from squid_py.ocean import Ocean
+#from squid_py.config import Config
 from smbus import SMBus
 
 
 app = Flask(__name__)
 bus = SMBus(1)
-ocean = Ocean(Config(filename='config.ini'))
+#ocean = Ocean(Config(filename='config.ini'))
 
 
 @app.route("/api/status", methods=['GET'])
@@ -19,17 +19,24 @@ def status():
 
 @app.route("/api/snap", methods=['GET'])
 def snap():
-    subprocess.call('flask_app/scripts/take_photos.sh')
+    #subprocess.call('flask_app/scripts/take_photos.sh')
+    #subprocess.call('/home/pi/git/pines-and-electronics/flask_app/scripts/take_photos.sh')
+    #this_cmd = "raspistill -t 10000 -tl 2000 -o /home/pi/git/pines-and-electronics/flask_app/images/image%04d.jpg"
+    #this_cmd = "raspistill -o cam.jpg"
+    this_cmd = "ls"
+    subprocess.call(this_cmd)
+
     return 'snap'
 
 
 @app.route("/api/register", methods=['POST'])
 def register():
-    labels = proccess_picture()
-    metadata =Metadata.get_example()
-    metadata['base']['tags'] = labels
-    ddo = ocean.assets.create(metadata, ocean.accounts._accounts[0])
-    return ddo.did
+    #labels = proccess_picture()
+    #metadata =Metadata.get_example()
+    #metadata['base']['tags'] = labels
+    #ddo = ocean.assets.create(metadata, ocean.accounts._accounts[0])
+    #return ddo.did
+    return "registered"
 
 
 @app.route("/api/snapshot", methods=['GET'])
@@ -39,9 +46,18 @@ def snapshot():
 
 @app.route("/api/linear", methods=['POST'])
 def linear():
-    bus.write_byte(8, ord(20))
+    bus.write_byte(8, ord("S"))
     return "linear"
 
+@app.route("/api/start", methods=['GET'])
+def start():
+    bus.write_byte(8, ord("S"))
+    return "START"
+
+@app.route("/api/stop", methods=['GET'])
+def stop():
+    bus.write_byte(8, ord("s"))
+    return "STOP"
 
 @app.route("/api/steer", methods=['POST'])
 def steer():
--- flask_app/scripts/take_photos.sh
+++ flask_app/scripts/take_photos.sh
@@ -1,2 +1,4 @@
 # Take photos using RPI camera
-raspistill -t 10000 -tl 2000 -o ./images/
+#raspistill -t 10000 -tl 2000 -o ./images/
+raspistill -t 10000 -tl 2000 -o /home/pi/git/pines-and-electronics/flask_app/images/image%04d.jpg
+
